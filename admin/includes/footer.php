22953b8cde785ed5cbeb194b8fc387589297ed0d
Angelica Evangelista <75600746+angelicaevangelista@users.noreply.github.com>
2024-01-14 15:50:01 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<!-- <div class="copyrights">
+	 <p>© 2016 PASTRYPLAY. All Rights Reserved |  <a href="#">PASTRYPLAY</a> </p>
+</div>	 -->
---
+++ admin/includes/header.php
@@ -0,0 +1,31 @@
+<div class="header-main">
+<div style="background: #3c2f2f; float: left; width: 65.25%; text-align: center; padding: 1em; margin-right: 1%;">
+								<h1 style="color: white;">PASTRYPLAY</h1>
+							</div>
+				
+						
+						<div style="float: left; width: 24.25%; background: #854442; text-align: center; padding: 0.3em 1em;">		
+								<ul>
+									<li class="dropdown profile_details_drop">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+											<div class="profile_img">	
+												<span class="prfil-img"><img src="images/User-icon.png" alt=""> </span> 
+												<div class="user-name">
+													<p>Welcome</p>
+													<span>Administrator</span>
+												</div>
+												<i class="fa fa-angle-down"></i>
+												<i class="fa fa-angle-up"></i>
+												<div class="clearfix"></div>	
+											</div>	
+										</a>
+										<ul class="dropdown-menu drp-mnu">
+											<li> <a href="change-password.php"><i class="fa fa-user"></i> Profile</a> </li> 
+											<li> <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a> </li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+							
+				     <div class="clearfix"> </div>	
+				</div>
